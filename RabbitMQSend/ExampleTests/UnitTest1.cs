25660ec3e8ee573a9bf2eefcd2a2409ee2cb368d
tony <tonymazn@gmail.com>
2017-07-07 17:40:46 -0600

    Test build

--- RabbitMQSend/ExampleTests/UnitTest1.cs
+++ RabbitMQSend/ExampleTests/UnitTest1.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ExampleTests
 {
     /// <summary>
-    /// Build Test
+    /// Build Test 1
     /// </summary>
     [TestClass]
     public class UnitTest1
