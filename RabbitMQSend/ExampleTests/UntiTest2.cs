1b8ec7b7cad1e5ca02ccceeb2596be2759d3158e
tony <tonymazn@gmail.com>
2017-07-07 18:54:50 -0600

    Add a new class

---
+++ RabbitMQSend/ExampleTests/UntiTest2.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleTests
+{
+    [TestClass]
+    public class UntiTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1 + 2, 4);
+        }
+    }
+}
